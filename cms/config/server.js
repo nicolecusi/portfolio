59abd0b8fab79ed1563272a8e654017b1045c365
superpoink <nicolecusi@gmail.com>
2020-11-24 15:55:36 -0500

    Removed strapi from this repo

--- cms/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '0005ca41c5108a1d70dc42f884311258'),
-    },
-  },
-});
